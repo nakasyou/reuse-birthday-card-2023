cd5004ddc5d674b756e7d27862756798ee41b4d2
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-08 12:22:23 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <div class="text-4xl text-center">
-        <div class="font-bold">Happy birthday,</div>
+        <div class="font-bold text-gradient-to-r from-red-500 to-indigo-500">Happy birthday,</div>
         <div>{ targetName }!!</div>
       </div>
     </div>
