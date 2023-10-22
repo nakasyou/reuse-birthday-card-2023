06305d734e442b0d44a9563e3ae877f67296d0b3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-10-22 12:01:47 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
           </div>
         )}
       </div>
-      <div class="bg-gradient-to-r from-red-500 to-indigo-500 w-screen h-screen">
+      <div class="bg-gradient-to-r from-red-500 to-indigo-500 w-screen h-screen touch-none overflow-hidden">
         <div class="flex items-center justify-center w-screen h-screen">
           <div class="text-4xl text-center">
             <div class="font-bold">Happy birthday,</div>
